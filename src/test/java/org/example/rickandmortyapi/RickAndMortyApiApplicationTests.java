0c80167c93240bbea0bcbed71127071f8dc5fdf7
Roman Romanenko <capital.car7@gmail.com>
2026-01-13 11:09:36 +0100

    Initial commit

---
+++ src/test/java/org/example/rickandmortyapi/RickAndMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rickandmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
